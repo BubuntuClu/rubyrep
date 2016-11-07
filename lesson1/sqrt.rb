6f75f4731c6c282135ca6941152bba1e0de5156f
aiidko <aiidko@mail.ru>
2016-11-07 22:57:15 +0500

    task 4 lesson 1

---
+++ lesson1/sqrt.rb
@@ -0,0 +1,22 @@
+puts "Enter the a value:"
+a = gets.chomp.to_i
+
+puts "Enter the b value:"
+b = gets.chomp.to_i
+
+puts "Enter the c value:"
+c = gets.chomp.to_i
+
+D = b**2 - 4 * a * c
+
+if (D > 0)
+	puts "discriminant: #{D}"
+	puts "x1: #{(-b + c)/(2 * a)}"
+	puts "x2: #{(-b - c)/(2 * a)}"
+else if (D == 0)
+		puts "discriminant: #{D}"
+		puts "x1=x2: #{-b/(2 * a)}"
+	else
+		puts " there are no values"
+	end
+end
