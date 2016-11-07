27d5e8c0e226b9f91c9cac13020f10e91c563e5c
aiidko <aiidko@mail.ru>
2016-11-07 22:48:54 +0500

    task 3 lesson 1. fix2

--- lesson1/pifagor.rb
+++ lesson1/pifagor.rb
@@ -4,18 +4,8 @@ firstS = gets.chomp.to_i
 puts "Enter the second side:"
 secondS = gets.chomp.to_i
 
-if (firstS > secondS)
-	maxSide = firstS
-else
-	maxSide = secondS
-end
-
 puts "Enter the third side:"
 thirdS = gets.chomp.to_i
-
-if (thirdS > maxSide)
-	maxSide = thirdS
-end
 
 if (firstS > secondS && firstS > thirdS)
 	if (firstS**2 == secondS**2 + thirdS**2)
