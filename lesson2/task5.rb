210cf5591e70094315edc72ed4c0a94063959b2c
aiidko <aiidko@mail.ru>
2016-11-10 21:11:45 +0500

    bug fix

--- lesson2/task5.rb
+++ lesson2/task5.rb
@@ -4,14 +4,14 @@ year = gets.chomp.to_i
 is_leap_year = (year % 4 == 0 && year % 400 == 0 )? true : false
 
 february = 28
-february = 29 if year % 4 == 0 && year % 100 !=0 || year % 400 == 0
+february = 29 if year % 4 == 0 && year % 100 != 0 || year % 400 == 0
 
 months = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 puts "Enter month:"
 month = gets.chomp.to_i
 
-while month > 12 && month < 0 do
+while month > 12 || month < 1 do
   puts "There is no such month. Enter month:"
   month = gets.chomp.to_i
 end
