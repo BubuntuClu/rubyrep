23e76e9569832021609af00fa5d382d34985eaf2
aiidko <aiidko@mail.ru>
2016-11-17 21:09:51 +0500

    modified 2 more classes

--- lesson6/cargo_car.rb
+++ lesson6/cargo_car.rb
@@ -1,15 +1,25 @@
+require_relative 'modules/validator'
+
 class CargoCar < Car
+  include Validator
+
   def initialize(type, capacity)
-    super(type)
     @capacity = capacity
+    super(type)    
   end
 
   def show_info
     puts "this car is #{type} and got capacity #{capacity} ton"
   end
 
+
   private
 
   #put this in private because i dont want let user use getter on capacity. they should use show_info
   attr_reader :capacity
+  def validate!
+    raise "NOT VALID TYPE!" if type !~ /^(cargo)$/i
+    raise "NOT VALID CAPACITY!" if capacity.to_i < 0
+    true
+  end
 end
--- lesson6/passenger_car.rb
+++ lesson6/passenger_car.rb
@@ -1,7 +1,11 @@
+require_relative 'modules/validator'
+
 class PassengerCar < Car
+  include Validator
+  
   def initialize(type, sleeps)
-    super(type)
     @sleeps = sleeps
+    super(type)
   end
 
   def show_info
@@ -12,6 +16,12 @@ class PassengerCar < Car
 
  #put this in private because i dont want let user use getter on sleeps. they should use show_info
   attr_reader :sleeps
+
+  def validate!
+    raise "NOT VALID TYPE!" if type !~ /^(passenger)$/i
+    raise "NOT VALID SLEEPS PLACE!" if sleeps.to_i < 10
+    true
+  end
 end
 
   
