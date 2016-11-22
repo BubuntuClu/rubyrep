d0ad3da975251085608885ee21bae0cc46618ce0
aiidko <aiidko@mail.ru>
2016-11-22 21:09:49 +0500

    1 part of task

---
+++ lesson9/modules/acessors.rb
@@ -0,0 +1,74 @@
+# module Acessors
+#   def attr_accessor_with_history(*args)
+#     args.each do |arg|
+#       var_name = "@#{arg}".to_sym
+
+#       define_method(arg) do 
+#         if instance_variable_defined?(var_name)
+#           instance_variable_get(var_name) 
+#         else
+#           instance_variable_set(var_name, []) 
+#         end
+#       end
+
+#       define_method("#{arg}=".to_sym) do |value|    
+#         n = var_name.size+1
+#         puts var_name
+#         instance_variable_set("@#{arg}", value) 
+#       end
+#     end
+#   end
+# end
+
+
+module Accessors
+  def attr_accessor_with_history(*args)
+    args.each do |arg|
+      define_method("#{arg}") do
+        if instance_variable_defined?("@#{arg}")
+          instance_variable_get("@#{arg}")
+        else
+          instance_variable_set("@#{arg}_history",[])
+          instance_variable_set("@#{arg}",0)
+        end
+      end
+
+      define_method("#{arg}=") do |val|
+        instance_variable_set("@#{arg}",val)
+        eval "@#{arg}_history+=[#{val}]"
+      end
+
+      define_method("#{arg}_history") do
+        instance_variable_get("@#{arg}_history")
+      end
+    end
+  end
+
+  def strong_attr_accessor(name,class_name)
+    define_method("#{name}") do
+      if instance_variable_defined?("@#{name}")
+        instance_variable_get("@#{name}")
+      else
+        instance_variable_set("@#{name}",0)
+      end
+    end
+
+    define_method("#{name}=") do |val|
+      if val.class.to_s == class_name.to_s
+        instance_variable_set("@#{name}",val)
+      else
+        raise "not valid type"
+      end
+    end
+  end
+
+end
+
+class Test
+  extend Accessors
+
+  attr_accessor_with_history :a, :b
+  strong_attr_accessor :c, String
+end
+
+
